48deeaa15b6c8b39fec26519b057d82556cde111
max h <hollismaximilian@gmail.com>
2021-01-15 23:45:38 -0500

    prevent arbitrary execution of scripts on client

--- Client/pages/_app.js
+++ Client/pages/_app.js
@@ -4,6 +4,7 @@ import GlobalStyle, { Theme } from '../src/globalStyles'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <script src="cssesc.js"></script>
       <GlobalStyle />
       <ThemeProvider theme={Theme}>
         <Component {...pageProps} />
