e5c77fdf24d0bca3a03c24fff5b8dcfd93b61b01
max h <hollismaximilian@gmail.com>
2021-01-21 23:53:59 -0500

    fixed bugs

--- Client/.next/static/chunks/webpack.js
+++ Client/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c0cfd44b0b742485b16b";
+/******/ 	var hotCurrentHash = "869d14a9fb76fbaef600";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
